67134749b06a0577f8824aa7ab1fafb8c1e2a0af
Naviganet <dadegi@gmail.com>
2026-06-29 11:57:26 +0200

    Codici lezione W9D1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// Questo è il primo file che parte all'avvio dell'applicazione: espone index.html nella rott principale, che è l'unico HTML esposto
+// import { StrictMode } from 'react'; Apporta al comportamento di React modifiche troppo coercitive, per questo lo togliamo
+import { createRoot } from 'react-dom/client';
+// L'importazione di index.css lo rende disponibile in tutta l'applicazione: la index.css contiene generalmente le impostazioni CSS di base valide per tutta l'applicazione
+import './index.css';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// L'alias createRoot genera il nodo principale, inserendolo nell'elemento con id root che trova nella index.html
+createRoot(document.getElementById('root')).render(<App />);
+
+// OGNI tag che non ha tag di chiusura, sia quelli standard sia quelli creati da noi (come <App />) devono necessariamente avere l'autochiusura, cioè lo slash prima di chiudere la parentesi
